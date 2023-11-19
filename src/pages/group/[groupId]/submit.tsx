c3e9337ad9db052feba9d5955624d23e03f98455
timonjagi <timothynjihia@gmail.com>
2023-11-19 08:50:10 +0300

    fix(components): fix build
    n
    
    BREAKING CHANGE: remove location check

--- src/pages/group/[groupId]/submit.tsx
+++ src/pages/group/[groupId]/submit.tsx
@@ -1,9 +1,9 @@
-import { Box, Text } from '@chakra-ui/react';
-import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import PageContent from '../../../components/Layout/PageContent';
-import NewPostForm from '../../../components/Community/Posts/PostForm/NewPostForm';
-import { auth } from '../../../firebase/clientApp';
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import PageContent from "../../../lib/layout/PageContent";
+import NewPostForm from "../../../lib/pages/group/Posts/PostForm/NewPostForm";
+import { auth } from "../../../lib/firebase/client";
 
 const SumbitPostPage: React.FC = () => {
   const [user] = useAuthState(auth);
@@ -11,7 +11,7 @@ const SumbitPostPage: React.FC = () => {
   return (
     <PageContent>
       <>
-        <Box padding='14px 0px' borderBottom='1px solid white'>
+        <Box padding="14px 0px" borderBottom="1px solid white">
           <Text>Create a post</Text>
         </Box>
         {user && <NewPostForm user={user} />}
