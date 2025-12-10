9e322f5236950823cd74327189fad630b6bd82a7
Timo Njagi <timothynjihia@gmail.com>
2025-12-10 21:34:19 +0300

    feat: Implement breed detail pages for user exploration, management, and public access.

--- src/lib/components/pages/breeds/PublicBreedDetailPage.tsx
+++ src/lib/components/pages/breeds/PublicBreedDetailPage.tsx
@@ -274,14 +274,13 @@ const PublicBreedDetailPage = () => {
                     emptyActionLabel="Add to Wishlist"
                     emptyActionIcon={<FiHeart />}
                     emptyAction={onAddToWishlist}
-                    columns={{ base: 1, md: 2, xl: 3 }}
+                    columns={{ base: 1, md: 2 }}
                   />
                 </TabPanel>
               </TabPanels>
             </Tabs>
           </SimpleGrid>
 
-          {!isMobile && <EthicalQuestionairreCard />}
 
         </Stack>
       </Container>
