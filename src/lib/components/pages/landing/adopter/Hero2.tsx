0b5e682000d01bdbf3f87d7ba39f4a2f5dd46a96
Timo Njagi <timothynjihia@gmail.com>
2025-12-10 22:02:46 +0300

    feat: introduce new Hero2 component for the adopter landing page.

--- src/lib/components/pages/landing/adopter/Hero2.tsx
+++ src/lib/components/pages/landing/adopter/Hero2.tsx
@@ -52,7 +52,7 @@ export default function Hero() {
             >
               <Button
                 as={Link}
-                href="/breeds"
+                href="/explore"
                 variant="primary"
                 color="brand.500"
                 backgroundColor="brand.500"
@@ -64,7 +64,7 @@ export default function Hero() {
                 fontSize="md"
                 fontWeight="bold"
               >
-                <Text color="white">View Breeds</Text>
+                <Text color="white">Explore Pets</Text>
               </Button>
               <HStack
                 as={Link}
