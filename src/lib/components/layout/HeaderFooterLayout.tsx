dbe0840e010503bde7027a13ad38e1687ccbc9ec
Timo Njagi <timothynjihia@gmail.com>
2025-12-13 19:49:43 +0300

    fix: remove unnecessary overflow properties from HeaderFooterLayout

--- src/lib/components/layout/HeaderFooterLayout.tsx
+++ src/lib/components/layout/HeaderFooterLayout.tsx
@@ -17,8 +17,6 @@ export const HeaderFooterLayout: React.FC<LayoutProps> = ({ children }) => {
       <Box
         as="main"
         h={{ base: `calc(100dvh - ${headerHeight})`, md: "100%" }}
-        overflow="scroll"
-        overflowY="auto"
         display="flex"
         flexDirection="column"
       >
